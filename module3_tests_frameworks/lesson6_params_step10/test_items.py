68231b2fff0ec616f7b616a39719c7d17be0bef7
Evgeniy Andreev <andrevgalex@gmail.com>
2024-04-02 22:05:26 +0300

    fix m3l6s10 (language)

--- module3_tests_frameworks/lesson6_params_step10/test_items.py
+++ module3_tests_frameworks/lesson6_params_step10/test_items.py
@@ -5,5 +5,11 @@ from selenium.webdriver.common.by import By
 
 def test_available_item(browser: function):
     link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    # link = " https://selenium1py.pythonanywhere.com/ru/catalogue/love_82/"
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    try:
+        browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > .btn-add-to-basket")
+        is_element_present = True
+    except:
+        is_element_present = False
+    assert is_element_present == True, "\nThe book is not available"
